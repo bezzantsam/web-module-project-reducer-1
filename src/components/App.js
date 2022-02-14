f42a8a82daafea0edab920d3b1764059c202ce24
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-02-14 06:55:12 -0800

    clear display

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import TotalDisplay from "./TotalDisplay";
 import CalcButton from "./CalcButton";
 import reducer, { initialState } from "./Reducer";
-import { applyNumber, changeOperation } from "./actions";
+import { applyNumber, changeOperation, CLEAR_DISPLAY } from "./actions";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -14,6 +14,9 @@ function App() {
   };
   const handleOperationClick = (operation) => {
     dispatch(changeOperation(operation));
+  };
+  const handleClearDisplay = () => {
+    dispatch(clearDisplay());
   };
 
   return (
@@ -78,7 +81,7 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton value={"CE"} onClick={handleClearDisplay} />
             </div>
           </form>
         </div>
